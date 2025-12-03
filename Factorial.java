9d3c933fb093773a4d2325c00f2cbec78669e8c4
SrijaVodela <srijavodela@gmail.com>
2025-12-03 13:32:35 +0530

    Fix factorial computation and output formatting

--- Factorial.java
+++ Factorial.java
@@ -1,18 +1,23 @@
-class Factorial{
-    public static long compute(int n){
-        if(n<0){
+class Factorial {
+
+    public static long compute(int n) {
+        if (n < 0) {
             System.out.println("Enter num greater than 0");
             return 0;
         }
-        for (int i=0;i<=n;i++){
-            fact*=i;
-        }
-        return fact;
 
+        long fact = 1; 
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
     }
-    public static void main(string args[]){
-        int n=5;
-        long ans=compute(n);
-        System.out.println("Factorial ",ans);
+
+    public static void main(String args[]) {
+        int n = 5;
+        long ans = compute(n);
+        System.out.println("Factorial: " + ans);
     }
 }
